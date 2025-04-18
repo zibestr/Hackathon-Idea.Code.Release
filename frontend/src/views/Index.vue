38b730f3ce5f868b365acab2fb555e84eedcddd5
GasilinEgor <eg.gasilin2018@yandex.ru>
2025-04-18 18:50:37 +0300

    create main page

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -3,8 +3,86 @@
 </script>
 
 <template>
-<h1>Добро пожаловать!</h1>
+<div class="main">
+    <img src="../assets/main_image.png" class="image">
+    <h1>The Neighborhood</h1>
+    <p>Сервис поиска людей</p>
+    <button>Найти соседа</button>
+</div>
 </template>
 
-<style>
+<style scoped>
+* {
+    padding: 0;
+    margin: 0;
+}
+
+.main {
+    width: 100%;
+    min-height: 967px;
+}
+
+.image {
+    position: absolute;
+    right: 0;
+    z-index: -10;
+}
+
+.main h1 {
+
+    width: 1132px;
+    height: 161px;
+
+    font-family: 'Mulish';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 128px;
+    line-height: 161px;
+    background-color: white;
+
+    position: relative;
+    top: 79px;
+    left: 40px;
+
+color: #000000;
+}
+
+.main p {
+    position: relative;
+    width: 553px;
+    height: 40px;
+    left: 40px;
+    top: 105px;
+
+    font-family: 'Mulish';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 40px;
+
+    color: #000000;
+}
+
+
+.main button {
+    position: relative;
+    width: 346px;
+    height: 60px;
+    left: 40px;
+    top: 204px;
+
+    background: #FF5E37;
+    border-radius: 45px;
+
+    font-family: 'Mulish';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 30px;
+
+color: #000000;
+
+
+
+}
 </style>
