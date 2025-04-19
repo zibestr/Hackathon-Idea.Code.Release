c726ff8095b0021809b59c0526bd7a1c14824417
KOROL_AD <adkorolkov@yandex.ru>
2025-04-19 16:55:21 +0300

    Вынесение сокетов в utils

--- project/utils/__init__.py
+++ project/utils/__init__.py
@@ -1,7 +1,10 @@
 from .logger import logs
 from .logger import setup_logger
+from .chat import router as chat_router
+
 
 __all__ = [
     'logs',
-    'setup_logger'
+    'setup_logger',
+    'chat_router'
 ]
