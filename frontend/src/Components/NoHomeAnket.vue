f456a0a622cf34715a4925345b87ecd61e387f58
GasilinEgor <eg.gasilin2018@yandex.ru>
2025-04-19 22:30:13 +0300

    second state of anket create page

---
+++ frontend/src/Components/NoHomeAnket.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="no_home_div">
+    <h3>Бюджет на съем жилья</h3>
+    <input placeholder="45000 руб">
+</div>
+</template>
+
+<style>
+input {
+    margin-top: 5px;    
+    width: 266px;
+    height: 40px;
+}
+
+h3 {
+    margin-top: 30px;
+}
+</style>
