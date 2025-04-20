43f8dd5c114a6b6d4e9598530a3d0d8ff13cfcf5
pyalexos <aligos@yandex.ru>
2025-04-20 05:15:02 +0300

    chto-to rabotaet, no zaprosy ya poka ne otpravlyal.

--- project/db/queries.py
+++ project/db/queries.py
@@ -1,8 +1,11 @@
 import redis.asyncio as redis
 import json
-from sqlmodel import select, and_, or_
+import requests
+from sqlmodel import select, and_, or_, alias, func
 from typing import Optional, List, Dict, Any
 import random
+from config import settings
+
 from db import (
     create_models,
     init_db,
@@ -27,28 +30,136 @@ from db import (
     Message
 )
 
+from utils import (
+    logs,
+    save_image,
+    ModelReadiness,
+    TextRequest,
+    ToxicityResponse,
+    RankingPairRequest,
+    RankingResponse,
+    NSFWRequest,
+    NSFWResponse,
+    Token,
+    TokenData,
+    UserData,
+    UserIdentify,
+    UserAuth
+)
+
 #TODO только один населенный пункт, анкета активна, ранжируется твоей морделью, если модель гг, то просто случайный порядок списка 
 #TODO приходит id (user листает ленту) -> если в user_response при request_user_id == user_id исключаем response_user_id из поиска, остальных пользователей
 #TODO для каждой пары json(user, searching_for user)
 # я кидаю дане запрос на получение кластера для нового пользователя и получаю кластер, записываю
-r = redis.Redis(host='localhost', port=6777, db=0, decode_responses=True)
+host, port = settings.redis.split(':')
+r = redis.Redis(host=host, port=port, db=0, decode_responses=True)
 
 
 async def get_user_by_email(email: str) -> Optional[User]:
     async with get_session() as session:
         result = await session.exec(
-            select(User).where(User.email == email)
+            select(User)
+            .where(User.email == email)
         )
         return result.first()
 
-async def create_user(fields: Dict[str, Any]) -> User:
-    # danya model
-    async for session in get_session():
-        user = User(**fields)
+
+async def create_user(user: UserAuth) -> Optional[User]:
+    data = user.about
+    ml_api_toxicity_state: ModelReadiness = requests.get(f'{settings.ml_api}/check_model_toxicity')
+    if data != '' and ml_api_toxicity_state.ready:
+        payload = dict(text=data)
+        response = requests.post(f'{settings.ml_api}/predict_toxicity', data=payload)
+        if response.status_code == 200:
+            if response.json()['non_toxicity'] < 0.6:
+                return
+
+    async with get_session() as session:
+        locality_id = await session.exec(
+            select(Locality.id)
+            .join(Region)
+            .where(
+                and_(
+                    Locality.name == user.locality_name,
+                    Region.title == user.region_name
+                )
+            )
+        )
+
+        ei_id = await session.exec(
+            select(EducationalInstitution.id)
+            .where(EducationalInstitution.short_name == user.educational_institution)
+        )
+        
+        ed_dir_id = await session.exec(
+            select(EducationDirection.id)
+            .where(EducationDirection.title == user.education_direction)
+        )
+        
+        user = User(
+            name=user.name,
+            age=user.age,
+            email=user.email,
+            phone=user.phone,
+            vk_id=user.vk_id,
+            about=user.about,
+            locality_id=locality_id.first(),
+            password_hash=user.hashed_password,
+            education_direction=ed_dir_id.first() if ed_dir_id.first() else None,
+            ei_id=ei_id.first() if ei_id.first() else None,
+            budget=user.budget,
+            gender=user.gender
+        )
+        
         session.add(user)
+        await session.flush()
+        
+        for photo in enumerate(user.photos):
+            photo_filename = await save_image(
+                user.id,
+                photo,
+                category='profile'
+            )
+            
+            photo = UserPhoto(
+                user_id=user.id,
+                file_name=photo_filename,
+            )
+            session.add(photo)
+        
+        if user.habits:
+            habits = await session.exec(select(BadHabit.id)
+                                        .where(
+                                            BadHabit.title.in_(user.habits)
+                                        )
+            )
+
+            habit_ids = habits.all()
+            for habit_id in habit_ids:
+                user_habit = t_user_bad_habits(
+                    user_id=user.id,
+                    bad_habits_id=habit_id
+                )
+                session.add(user_habit)
+        
+        if user.interests:
+            interests = await session.exec(select(Interest.id)
+                                           .where(
+                                               Interest.title.in_(user.interests)
+                                           )
+            )
+
+            interest_ids = interests.all()
+            for interest_id in interest_ids:
+                user_interest = t_user_interest(
+                    user_id=user.id,
+                    interest_id=interest_id
+                )
+                session.add(user_interest)
+        
         await session.commit()
-        await session.refresh(user)
-        return user
+        return user.id
+
 
 async def update_user(user_id: int, update_data: Dict[str, Any]) -> Optional[User]:
     # danya model
@@ -65,11 +176,13 @@ async def update_user(user_id: int, update_data: Dict[str, Any]) -> Optional[Use
         await session.refresh(user)
         return user
 
+
 async def check_password(user_email: str, user_password: str) -> bool:
     user = await get_user_by_email(user_email)
     if not user:
         return False
     return user.verify_password(user_password)
+
 
 # как получить id текущего пользователя который отправил запрос
 async def get_recomendations(user_id: int) -> List[Dict[str, Any]]:
@@ -202,10 +315,31 @@ async def update_habitation(habitation_id: int, new_name: str) -> Optional[Habit
         await session.commit()
         await session.refresh(habitation)
         return habitation
+    
+
+async def get_matches(user_id: int):
+    async with get_session() as session:
+        user_matches = await session.exec(
+            select(UserResponse.response_user_id).where(
+                UserResponse.request_user_id == user_id).join(
+                    Match, Match.response_id == UserResponse.id
+            )
+        )
+        return user_matches.all()
+
+
+async def get_all_matches():
+    async with get_session() as session:
+        all_user_matches = await session.exec(
+
+        )
+        return all_user_matches.all()
       
+
 async def store_user_relation(hash_name: str, user_id: int, related_ids: list[int]) -> None:
     value = json.dumps(related_ids)
     await r.hset(hash_name, user_id, value)
+
 
 async def get_user_relation(hash_name: str, user_id: int) -> list[int]:
     value = await r.hget(hash_name, user_id)
--- project/main.py
+++ project/main.py
@@ -1,29 +1,31 @@
 import uvicorn
 from typing import AsyncGenerator
 from fastapi import FastAPI, Request, HTTPException, Depends, status
-from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from typing import Dict
-import uuid
 
 from config import settings
 from db import init_db
+from chat import router as chat_router
+from auth import router as auth_router
 from auth import (
     authenticate_user,
     create_access_token,
     get_current_active_user
 )
-from utils import (setup_logger,
-                   logs,
-                   ModelReadiness,
-                   TextRequest,
-                   RankingPairRequest,
-                   NSFWRequest,
-                   Token,
-                   TokenData,
-                   User,
-                   UserInDB
+
+from utils import (
+    setup_logger,
+    logs,
+    ModelReadiness,
+    TextRequest,
+    RankingPairRequest,
+    NSFWRequest,
+    Token,
+    TokenData,
+    UserData,
+    UserAuth
 )
 
 
@@ -33,53 +35,24 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
    yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
+)
 
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: Dict[str, WebSocket] = {}
-        self.usernames: Dict[str, str] = {}  # {user_id: username}
+app.add_middleware(
+    CORSMiddleware,
+    #allow_origins=settings.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-    async def connect(self, websocket: WebSocket, user_id: str):
-        await websocket.accept()
-        self.active_connections[user_id] = websocket
-
-    async def register_user(self, user_id: str, username: str):
-        self.usernames[user_id] = username
-        print("{} Онлайн".format(username))
-
-    async def disconnect(self, user_id: str):
-        if user_id in self.usernames:
-            username = self.usernames[user_id]
-            del self.active_connections[user_id]
-            del self.usernames[user_id]
-            print("{} не онлайн".format(username))
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections.values():
-            await connection.send_text(message)
-
-manager = ConnectionManager()
-
-
-@logs
-@app.post("/auth", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.get("me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
+app.include_router(auth_router, prefix="/api")
+app.include_router(chat_router, prefix="/api/chat", tags=["chat"])
 
 
 @logs
@@ -94,33 +67,6 @@ async def app_exception_handler(request: Request, exc: HTTPException):
             }
         },
     )
-
-
-#TODO MOVE route
-# @app.get("/")
-# async def get(request: Request):
-#     return {"request": request}
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    user_id = str(uuid.uuid4())
-    await manager.connect(websocket, user_id)
-    
-    try:
-        username = await websocket.receive_text()
-        await manager.register_user(user_id, username)
-        
-        while True:
-            message = await websocket.receive_text()
-            await manager.broadcast(f"{username}: {message}")
-            
-    except WebSocketDisconnect:
-        await manager.disconnect(user_id)
-    except Exception as e:
-        print(f"Ошибка: {e}")
-        await manager.disconnect(user_id)
-
-
 
 
 if __name__ == '__main__':
