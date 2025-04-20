c0d5eef78d0acb769ea25296ba77373a79a164ea
GasilinEgor <eg.gasilin2018@yandex.ru>
2025-04-20 10:38:23 +0300

    create recomedations page and microupdates

---
+++ frontend/src/Components/RecHome.vue
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<template>
+<div class="rec_home_div">
+    <h2>Жилье</h2>
+    <img src="../assets/state_room.jpg">
+    <div class="home_info">
+    <p>65000 руб./мес.</p>
+    <p>Коммуналка включена, без залога</p>
+</div>
+</div>
+</template>
+
+<style>
+.rec_home_div {
+    width: 90%;
+}
+
+.rec_home_div img {
+    width: 620px;
+}
+
+.rec_home_div h2 {
+    font-size: 32px;
+    margin-bottom: 20px;
+}
+
+.rec_home_div p {
+    font-size: 28px;
+}
+
+.home_info {
+    margin: 10px;
+    margin-bottom: 20px;
+}
+</style>
---
+++ frontend/src/views/Recomendations.vue
@@ -0,0 +1,13 @@
+<script setup>
+import PersonRec from '../Components/RecPerson.vue'
+</script>
+
+<template>
+<div class="rec_div">
+    <PersonRec />
+</div>
+</template>
+
+<style>
+
+</style>
